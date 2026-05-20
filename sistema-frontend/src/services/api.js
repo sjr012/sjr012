5573258074209e110482e55d3e75b5ccfff77a09
Josuel Júnior <josuel_sjr@outlook.com>
2026-05-20 14:42:26 -0300

    correção do login na API

--- sistema-frontend/src/services/api.js
+++ sistema-frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://sjr012-production.up.railway.app";
+const API_URL = process.env.REACT_APP_API_URL || "https://sjr012.onrender.com";
 
 export function getAuthHeaders() {
   const token = localStorage.getItem("token");
