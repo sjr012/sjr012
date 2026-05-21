8694d10ece940f85232b3c293605e51c599ce99a
Josuel Júnior <josuel_sjr@outlook.com>
2026-05-21 01:58:25 -0300

    fix auth.controller

--- src/main/java/com/sistema/controller/AuthController.java
+++ src/main/java/com/sistema/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sistema.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.sistema.dto.LoginResponse;
 import com.sistema.model.Usuario;
 import com.sistema.repository.UsuarioRepository;
@@ -10,27 +11,36 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "/login")
+@CrossOrigin(origins = "https://sistema-frontend-orjl.onrender.com")
 public class AuthController {
 
     private final UsuarioRepository repo;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthController(
             UsuarioRepository repo,
-            JwtService jwtService) {
+            JwtService jwtService,
+            PasswordEncoder passwordEncoder) {
         this.repo = repo;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody Usuario u) {
+    public ResponseEntity<?> login(@RequestBody Usuario u) {
+
         return repo.findByLogin(u.getLogin())
-                .filter(usuario -> u.getSenha().equals(usuario.getSenha()))
-                .<ResponseEntity<Object>>map(usuario -> {
+                .filter(usuario -> passwordEncoder.matches(
+                        u.getSenha(),
+                        usuario.getSenha()))
+                .<ResponseEntity<?>>map(usuario -> {
+
                     String token = jwtService.gerarToken(usuario);
-                    return ResponseEntity.ok(new LoginResponse(token, usuario));
+
+                    return ResponseEntity.ok(
+                            new LoginResponse(token, usuario));
                 })
-                .orElse(ResponseEntity.status(401).body("Login errado"));
+                .orElse(ResponseEntity.status(401).body("Login inválido!"));
     }
 }
