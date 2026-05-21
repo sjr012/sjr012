cf6b5fceae24935e98fb33cb351ac94850d2dd6a
Josuel Júnior <josuel_sjr@outlook.com>
2026-05-21 01:32:51 -0300

    fix role admin uppercase

--- src/main/java/com/sistema/security/SecurityConfig.java
+++ src/main/java/com/sistema/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login").permitAll()
 
                         // Somente ADMIN
-                        .requestMatchers("/usuarios/**").authenticated()
+                        .requestMatchers("/usuarios/**").hasRole("ADMIN")
                         .requestMatchers("/clientes/**").hasRole("ADMIN")
                         .requestMatchers("/funcionarios/**").hasRole("ADMIN")
 
