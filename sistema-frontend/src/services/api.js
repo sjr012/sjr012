c9f3e664490823edfd0e896339938aed4aaefef8
Josuel Júnior <josuel_sjr@outlook.com>
2026-05-22 01:19:47 -0300

    correção da API 2

--- sistema-frontend/src/services/api.js
+++ sistema-frontend/src/services/api.js
@@ -1,34 +1,36 @@
-const API_URL = process.env.REACT_APP_API_URL || "https://sjr012.onrender.com";
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://sjr012.onrender.com";
 
 export function getAuthHeaders() {
+
   const token = localStorage.getItem("token");
 
   return {
     "Content-Type": "application/json",
     Authorization: token ? `Bearer ${token}` : ""
   };
+}
 
-  export async function apiFetch(url, options = {}) {
+export async function apiFetch(url, options = {}) {
 
-    const response = await fetch(url, {
-      ...options,
-      headers: {
-        ...getAuthHeaders(),
-        ...(options.headers || {})
-      }
-    });
-
-    if (response.status === 401) {
-
-      localStorage.removeItem("token");
-      localStorage.removeItem("usuario");
-
-      window.location.href = "/";
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      ...getAuthHeaders(),
+      ...(options.headers || {})
     }
+  });
 
-    return response;
+  if (response.status === 401) {
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+
+    window.location.href = "/";
   }
-  
+
+  return response;
 }
 
 export default API_URL;
