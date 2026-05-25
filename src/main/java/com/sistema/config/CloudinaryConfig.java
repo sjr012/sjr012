2a8dd6ab488e3319e1a7f446b8a875262a6e2564
Josuel Júnior <josuel_sjr@outlook.com>
2026-05-25 14:38:58 -0300

    configurando os anexos em outro repositório.

---
+++ src/main/java/com/sistema/config/CloudinaryConfig.java
@@ -0,0 +1,20 @@
+package com.sistema.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(Map.of(
+                "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
+                "api_key", System.getenv("CLOUDINARY_API_KEY"),
+                "api_secret", System.getenv("CLOUDINARY_API_SECRET"),
+                "secure", true));
+    }
+}
